7a6f1f8cca139e81f6c428b64409d7fcb42fd115
Abhishek Khatri <abhishekkhatri98@gmail.com>
2022-10-13 20:43:45 -0400

    completed lab 05

--- Labs/Lab 05/helpers.js
+++ Labs/Lab 05/helpers.js
@@ -1,11 +1,46 @@
 //You can add and export any helper functions you want here. If you aren't using any, then you can just leave this file as is.
 const axios = require("axios");
 
+const isNumberChar = (char) => char >= "0" && char <= "9";
+
 const GET = async (url) => {
   const { data } = await axios.get(url);
   return data;
 };
 
+const errResp = (e, res, notFoundMsg = null) => {
+  if (e.response && e.response.status && e.response.statusText) {
+    res.status(e.response.status).json({
+      error:
+        e.response.status === 404 && notFoundMsg
+          ? notFoundMsg
+          : e.response.statusText,
+    });
+  } else res.status(500);
+};
+
+const isValidId = (idParam) => {
+  const errObj = {
+    response: {
+      status: 400,
+      statusText: "Invalid URL Parameter",
+    },
+  };
+  if (!idParam || typeof idParam !== "string" || idParam.trim().length === 0)
+    throw errObj;
+  idParam
+    .trim()
+    .split("")
+    .forEach((char) => {
+      if (!isNumberChar(char)) throw errObj;
+    });
+  const id = parseInt(idParam);
+  if (!isFinite(id) || id < 1) throw errObj;
+  return idParam.trim();
+};
+
 module.exports = {
   GET,
+  errResp,
+  isValidId,
 };
--- Labs/Lab 05/routes/pokemon.js
+++ Labs/Lab 05/routes/pokemon.js
@@ -2,24 +2,26 @@
 //Lecture Code Refernece -> https://github.com/stevens-cs546-cs554/CS-546/tree/master/lecture_05/code/routes
 const express = require("express");
 const { pokemon } = require("../data");
+const { errResp, isValidId } = require("../helpers");
 
 const router = express.Router();
 
 router.route("/:id").get(async (req, res) => {
   try {
-    const pokemonData = await pokemon.pokemonById(req.params.id);
+    const pokeId = isValidId(req.params.id);
+    const pokemonData = await pokemon.pokemonById(pokeId);
     res.json(pokemonData);
   } catch (e) {
-    res.status(e.response.status).json({ message: e.response.statusText });
+    errResp(e, res, "Pokémon Not Found!");
   }
 });
 
-router.route("/").get(async (req, res) => {
+router.route("/").get(async (_req, res) => {
   try {
     const pokemonData = await pokemon.pokemon();
     res.json(pokemonData);
   } catch (e) {
-    res.status(e.response.status).json({ message: e.response.statusText });
+    errResp(e, res);
   }
 });
 
